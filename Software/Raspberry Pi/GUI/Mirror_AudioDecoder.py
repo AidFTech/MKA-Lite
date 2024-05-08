c7dbdfacdea37b7f36b88299a60d28bad32ad91e
Aidan Franits <58407173+AidFTech@users.noreply.github.com>
2024-05-08 14:21:09 -0500

    Indentation
    Fixed indentation issues.

--- Software/Raspberry Pi/GUI/Mirror_AudioDecoder.py
+++ Software/Raspberry Pi/GUI/Mirror_AudioDecoder.py
@@ -4,39 +4,41 @@ from subprocess import Popen
 
 class AudioDecoder (object):
 
-    SOCKET_PATH = '/run/mka_audio.sock'
+	SOCKET_PATH = '/run/mka_audio.sock'
 
-    def __init__(self):
-        self.decoder = None
-        if path.exists(self.SOCKET_PATH):
-            remove(self.SOCKET_PATH)
-        self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        self.socket.bind(self.SOCKET_PATH)
+	def __init__(self):
+		self.decoder = None
+		if path.exists(self.SOCKET_PATH):
+			remove(self.SOCKET_PATH)
+		self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		self.socket.bind(self.SOCKET_PATH)
 
-        def start(self):
-            self.decoder = Popen(
-                [
-                    "ffplay", 
-                    "-f",
-                    "s16le",
-                    "-ac",
-                    "2",
-                    "-ar",
-                    "44100",
-                    "-nodisp",
-                    'unix:%s' % self.SOCKET_PATH
-                ],
-            )
+	def start(self):
+		self.decoder = Popen(
+			[
+				"ffplay", 
+				"-f",
+				"s16le",
+				"-ac",
+				"2",
+				"-ar",
+				"44100",
+				"-nodisp",
+				'unix:%s' % self.SOCKET_PATH
+			],
+		)
 
-        def stop(self):
-            if self.decoder is not None:
-                self.decoder.terminate()
-                self.socket.close()
-                remove(self.SOCKET_PATH)
+	def stop(self):
+		if self.decoder is not None:
+			self.decoder.terminate()
+			self.socket.close()
+			remove(self.SOCKET_PATH)
 
-        def sendAudioData(self, data: bytes):
-            if self.decoder is not None:
-                self.socket.send(data)
+			self.decoder = None
 
-        def running(self) -> bool:
-            return self.decoder is not None
+	def sendAudioData(self, data: bytes):
+		if self.decoder is not None:
+			self.socket.send(data)
+
+	def running(self) -> bool:
+		return self.decoder is not None
