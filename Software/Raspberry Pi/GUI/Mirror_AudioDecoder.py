8dd50cee48df0c16c270673dcc561987829feb7a
Ted Salmon <tass2001@gmail.com>
2024-05-09 00:31:53 -0400

    [FIX] Software: Thread the socket handler

--- Software/Raspberry Pi/GUI/Mirror_AudioDecoder.py
+++ Software/Raspberry Pi/GUI/Mirror_AudioDecoder.py
@@ -1,10 +1,13 @@
 import socket
+import threading
 from os import path, remove
 from subprocess import Popen
 
-class AudioDecoder (object):
+class AudioDecoder(object):
 
     SOCKET_PATH = '/run/mka_audio.sock'
+
+    data = []
 
     def __init__(self):
         self.decoder = None
@@ -13,6 +16,7 @@ class AudioDecoder (object):
         self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.socket.bind(self.SOCKET_PATH)
         self.socket.listen(1)
+        threading.Thread(target=self.acceptClient)
 
     def start(self):
         self.decoder = Popen(
@@ -28,22 +32,28 @@ class AudioDecoder (object):
                 'unix:%s' % self.SOCKET_PATH
             ],
         )
-        not_bound = True
-        while not_bound:
-            self.socket.accept()
-            not_bound = False
+
+    def acceptClient(self):
+        while True:
+            conn, _ = self.socket.accept()
+            threading.Thread(target=self.handleClient, args=(conn,))
+
+    def handleClient(self, conn):
+        while True:
+            for item in self.data:
+                conn.send(item)
+
 
     def stop(self):
         if self.decoder is not None:
             self.decoder.terminate()
             self.socket.close()
             remove(self.SOCKET_PATH)
-
             self.decoder = None
 
     def sendAudioData(self, data: bytes):
         if self.decoder is not None:
-            self.socket.send(data)
+            self.data.append(data)
 
     def running(self) -> bool:
         return self.decoder is not None
