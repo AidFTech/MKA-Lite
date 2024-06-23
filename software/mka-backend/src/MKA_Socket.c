749b7cf3975715416bf1a752faba7f9656449c6a
Ted Salmon <tass2001@gmail.com>
2024-06-22 22:06:56 -0400

    [WIP] mka-backend: Upend the whole thing

--- software/mka-backend/src/MKA_Socket.c
+++ software/mka-backend/src/MKA_Socket.c
@@ -1,86 +1,92 @@
 #include "MKA_Socket.h"
 
-//Create a new socket message.
-Socket_Message* createSocketMessage(const uint8_t opcode, const uint16_t length) {
-    Socket_Message* the_return;
-    the_return = (Socket_Message*) malloc(sizeof(Socket_Message));
+// Create a new socket message.
+SocketMessage *SocketCreateMessage(const uint8_t opcode, const uint16_t length)
+{
+    SocketMessage * message = (SocketMessage*) malloc(sizeof(SocketMessage));
 
-    the_return->opcode = opcode;
-    the_return->l = length;
+    message->opcode = opcode;
+    message->len = length;
+    message->data = (uint8_t *) malloc(length*sizeof(uint8_t));
 
-    the_return->data = (uint8_t*) malloc(length*sizeof(uint8_t));
-
-    return the_return;
+    return message;
 }
 
-//Clear a socket message from memory.
-void clearSocketMessage(Socket_Message* message) {
+// Clear a socket message from memory.
+void SocketClearMessage(SocketMessage* message)
+{
     free(message->data);
     free(message);
 }
 
-//Populate a socket message with data bytes.
-void fillSocketMessageBytes(Socket_Message* message, uint8_t* data) {
-    for(unsigned int i=0;i<message->l;i+=1)
-        message->data[i] = data[i];
+// Populate a socket message with data bytes.
+void SocketFillMessageBytes(SocketMessage* message, uint8_t* data)
+{
+    memcpy(message->data, data, message->len);
 }
 
-//Refresh a socket message.
-void refreshSocketMessage(Socket_Message* message, const uint8_t opcode, const uint16_t length) {
+// Refresh a socket message.
+void SocketRefreshMessage(SocketMessage* message, const uint8_t opcode, const uint16_t length)
+{
     message->opcode = opcode;
-    message->l = length;
+    message->len = length;
 
     free(message->data);
-    message->data = (uint8_t*)malloc(length*sizeof(uint8_t));
+    message->data = (uint8_t *) malloc(length * sizeof(uint8_t));
 }
 
-//Create a new socket with server and client ports.
-MKA_Socket* createSocket() {
+// Create a new socket with server and client ports.
+MKASocket* SocketCreate()
+{
     remove(SOCKET_PATH);
 
-    MKA_Socket* the_return;
-    the_return = (MKA_Socket*) malloc(sizeof(MKA_Socket));
+    MKASocket* sock = (MKASocket*) malloc(sizeof(MKASocket));
 
-    the_return->server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
-    
-    struct sockaddr_un server_address;
-    server_address.sun_family = AF_UNIX;
-    strcpy(server_address.sun_path, SOCKET_PATH);
+    sock->server = socket(AF_UNIX, SOCK_STREAM, 0);
 
-    bind(the_return->server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-    listen(the_return->server_socket, 5);
+    struct sockaddr_un path;
+    path.sun_family = AF_UNIX;
+    strcpy(path.sun_path, SOCKET_PATH);
 
-    the_return->client_socket = accept(the_return->server_socket, NULL, NULL);
+    int res = bind(sock->server, (struct sockaddr *) &path, sizeof(path));
+    listen(sock->server, 5);
 
-    return the_return;
+    //the_return->clients = accept(the_return->server_socket, NULL, NULL);
+
+    return sock;
 }
 
 //Clear the socket from memory.
-void clearSocket(MKA_Socket* socket) {
+void SocketClose(MKASocket* socket)
+{
     //TODO: Close the sockets?
     free(socket);
 }
 
 //Read binary data from the socket.
-ssize_t readSocketBytes(MKA_Socket* socket, uint8_t* data, const int length) {
-    return recv(socket->client_socket, data, length, 0);
+ssize_t SocketReadBytes(MKASocket* socket, uint8_t* data, const int length)
+{
+    return recv(socket->client, data, length, 0);
 }
 
 //Write binary data to the socket.
-void writeSocketBytes(MKA_Socket* socket, uint8_t* data, const int length) {
-    send(socket->client_socket, data, length, 0);
+void SocketWriteBytes(MKASocket* socket, uint8_t* data, const int length)
+{
+    send(socket->client, data, length, 0);
 }
 
-//Read a message from the socket.
-int readSocketMessage(MKA_Socket* socket, Socket_Message* message, const int length) {
+// Read a message from the socket.
+int SocketReadMessage(MKASocket* socket, SocketMessage* message, const int length) {
     uint8_t read_data[length];
-    const int message_size = readSocketBytes(socket, read_data, length);
+    const int message_size = SocketReadBytes(socket, read_data, length);
 
-    if(message_size <= 0)
+    if (message_size <= 0) {
         return message_size;
+    }
 
-    if(message_size < sizeof(SOCKET_START) + 2)
+    if (message_size < sizeof(SOCKET_START) + 2) {
         return -1;
+    }
 
     for(uint8_t i=0;i<sizeof(SOCKET_START)-1;i+=1) {
         if(read_data[i] != (uint8_t)SOCKET_START[i])
@@ -89,41 +95,45 @@ int readSocketMessage(MKA_Socket* socket, Socket_Message* message, const int len
 
     const uint8_t opcode = read_data[sizeof(SOCKET_START) - 1], msg_length = read_data[sizeof(SOCKET_START)]-1;
 
-    refreshSocketMessage(message, opcode, msg_length);
+    SocketRefreshMessage(message, opcode, msg_length);
     for(uint8_t i=0;i<msg_length;i+=1)
         message->data[i] = read_data[sizeof(SOCKET_START) + 1 + i];
 
     return msg_length;
 }
 
-//Write a message to the socket.
-void writeSocketMessage(MKA_Socket* socket, Socket_Message* message) {
-    if(message->l + 1 > 255)
+// Write a message to the socket.
+void SocketWriteMessage(MKASocket *socket, SocketMessage *message)
+{
+    if(message->len + 1 > SOCKET_MAX_DATA_SIZE) {
         return;
+    }
 
-    uint8_t write_data[message->l + 10];
+    uint8_t write_data[message->len + 10];
 
     for(uint8_t i=0;i<sizeof(SOCKET_START)-1;i+=1)
         write_data[i] = SOCKET_START[i];
 
     write_data[7] = message->opcode;
-    write_data[8] = (uint8_t)(message->l + 1);
+    write_data[8] = (uint8_t)(message->len + 1);
 
-    for(int i=0;i<message->l;i+=1)
+    for(int i = 0; i < message->len; i+=1) {
         write_data[i+9] = message->data[i];
+    }
 
     uint8_t checksum = 0;
-    for(uint8_t i=0;i<sizeof(write_data)-1;i+=1)
+    for(uint8_t i = 0; i < sizeof(write_data) - 1; i += 1) {
         checksum ^= write_data[i];
+    }
 
     write_data[sizeof(write_data)-1] = checksum;
 
-    writeSocketBytes(socket, write_data, message->l + 10);
+    SocketWriteBytes(socket, write_data, message->len + SOCKET_FRAME_SIZE);
 }
 
 //Write an IBus message to the socket.
-void writeIBusToSocket(MKA_Socket* socket, IBus_Message* message) {
-    Socket_Message* sock_message = createSocketMessage(0x68, message->l + 4);
+void writeIBusToSocket(MKASocket* socket, IBus_Message* message) {
+    SocketMessage* sock_message = createSocketMessage(0x68, message->l + 4);
 
     uint8_t sock_bytes[message->l + 4];
     getBytes(message, sock_bytes);
